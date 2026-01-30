40bd86c62677efbb1d8f84468d2c250f3d339aa4
PirorumSam <162376040+PirorumSam@users.noreply.github.com>
2026-01-30 15:04:49 +0700

    Git_Assignment_01: Create bye button (#2)

--- Assignment/Assignment/MainWindow.xaml.cs
+++ Assignment/Assignment/MainWindow.xaml.cs
@@ -25,5 +25,9 @@ namespace Assignment
         {
 			MessageBox.Show("xin chào");
 		}
+		private void btnBye_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("tạm biệt");
+		}
 	}
 }
